4813e45f7607731ae2bb1c0d7707295eb7289c89
plotnikovsp48 <plotnikovsp48@yandex.ru>
2020-08-12 17:26:07 +0300

    Updates

---
+++ precache-manifest.47d97bc99f8bc2d94dc0454b56949389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0474c2426005b642981843dbeec38a72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab392482ba6d8373b59d",
+    "url": "/static/js/2.958ae10e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.958ae10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f94da48b66d04e09b6",
+    "url": "/static/js/main.892cbeb7.chunk.js"
+  },
+  {
+    "revision": "76d831bbf7fd3ee97e21",
+    "url": "/static/js/runtime-main.6475b5bb.js"
+  }
+]);
